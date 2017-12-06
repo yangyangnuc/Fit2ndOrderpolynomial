514bce0df4ba44a9e64dd5f10e71ef00117ae050
Alex <695854799@qq.com>
2017-12-06 15:31:23 +0800

    add Least matrix method for comparison

--- Fit2ndOrderpolynomial.m
+++ Fit2ndOrderpolynomial.m
@@ -16,4 +16,45 @@ yDiff = y1 - y;
 figure,subplot 221,plot(x,yDiff),subplot 222, plot(x,y),subplot 223, plot(x,y1);
 max(yDiff),min(yDiff)
 
+
+
+
+
+
+%% using Least square matrix method
+% 1st step, create LS matrix
+% input, vector x, vector y, order of polynomial
+% output, LS matrix
+order = 2;
+leastSquareMatrix  = zeros(order + 1,order + 1);
+for i=1:size(x,1)
+   for r=1:order+1
+      for c=1:order+1
+          leastSquareMatrix(r,c) = leastSquareMatrix(r,c) ...
+          + power(x(i),(r+c));
+      end
+      leastSquareMatrix(r, order + 1) = leastSquareMatrix(r,c) + x(i) ...
+          * power(y(i), r);
+   end   
+end
+
+bestFitMatrix = leastSquareMatrix
+
+% Solve Linear Equation
+% input is bestFitMatrix
+
+bestFitMatrix_rowCnt = size(bestFitMatrix,1);
+if bestFitMatrix_rowCnt*(bestFitMatrix_rowCnt + 1) != length()
+    
+end
+
+
+
+
+
+
+
+
+
+
 disp('********* Game Over ***********');
